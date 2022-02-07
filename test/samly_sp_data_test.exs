34fdbe1a28fa18c3248f3142459d96c37e8e5310
Bernard Duggan <bernard@circl.es>
2022-02-07 17:07:27 +1100

    Allow inline certs and keys for SPs

--- test/samly_sp_data_test.exs
+++ test/samly_sp_data_test.exs
@@ -9,6 +9,13 @@ defmodule SamlySpDataTest do
     keyfile: "test/data/test.pem"
   }
 
+  @sp_config2 %{
+    id: "sp1",
+    entity_id: "urn:test:sp1",
+    certificate: File.read!("test/data/test.crt"),
+    key: File.read!("test/data/test.pem")
+  }
+
   test "valid-sp-config-1" do
     %SpData{} = sp_data = SpData.load_provider(@sp_config1)
     assert sp_data.valid?
@@ -24,6 +31,25 @@ defmodule SamlySpDataTest do
     sp_config = %{@sp_config1 | keyfile: ""}
     %SpData{key: :undefined} = sp_data = SpData.load_provider(sp_config)
     assert sp_data.valid?
+  end
+
+  test "inline cert and key" do
+    %SpData{} = sp_data = SpData.load_provider(@sp_config2)
+    assert sp_data.valid?
+    assert sp_data.cert != :undefined
+    assert sp_data.key != :undefined
+  end
+
+  test "invalid inline cert" do
+    sp_config = %{@sp_config2 | id: "bad cert", certificate: "Bad Certificate"}
+    %SpData{} = sp_data = SpData.load_provider(sp_config)
+    refute sp_data.valid?
+  end
+
+  test "invalid inline key" do
+    sp_config = %{@sp_config2 | id: "bad key", key: "Bad Key"}
+    %SpData{} = sp_data = SpData.load_provider(sp_config)
+    refute sp_data.valid?
   end
 
   test "invalid-sp-config-1" do
@@ -50,9 +76,8 @@ defmodule SamlySpDataTest do
     refute sp_data.valid?
   end
 
-  @tag :skip
   test "invalid-sp-config-5" do
-    sp_config = %{@sp_config1 | keyfile: "test/data/test.crt"}
+    sp_config = %{@sp_config1 | keyfile: "test/data/bad_key.pem"}
     %SpData{} = sp_data = SpData.load_provider(sp_config)
     refute sp_data.valid?
   end
